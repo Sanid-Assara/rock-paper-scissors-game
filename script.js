f33da9d91515034f2711f62675992d29ae4a583f
Sanid-Assara <sanid.assara@gmail.com>
2024-06-05 11:42:16 +0200

    Make if statment to determine the winner based on the rules. Print the result to the console. Add comments to the code.

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+//Player 1 choice with process.argv input
 let player1Choice = process.argv[2].toLowerCase();
 
+//Computer choice with Math.random() method and switch statment
 let randomNumber = Math.floor(Math.random() * 3) + 1;
 let computerChoice;
+
 switch (randomNumber) {
   case 1:
     computerChoice = "rock";
@@ -15,4 +18,27 @@ switch (randomNumber) {
     computerChoice = "scissors";
 
     break;
+}
+
+//if statment determine the winner based on the rules and print the result to the console
+if (
+  (player1Choice === "rock" && computerChoice === "scissors") ||
+  (player1Choice === "scissors" && computerChoice === "paper") ||
+  (player1Choice === "paper" && computerChoice === "rock")
+) {
+  console.log(
+    `You chose ${player1Choice} and the computer chose ${computerChoice}. You win!`
+  );
+} else if (
+  (player1Choice === "scissors" && computerChoice === "rock") ||
+  (player1Choice === "paper" && computerChoice === "scissors") ||
+  (player1Choice === "rock" && computerChoice === "paper")
+) {
+  console.log(
+    `You chose ${player1Choice} and the computer chose ${computerChoice}. You lose!`
+  );
+} else {
+  console.log(
+    `You chose ${player1Choice} and the computer chose ${computerChoice}. It's a draw!`
+  );
 }
